e8d4b85625396fae669dc7a79bb3df682ae03375
rilarchsen <rilarchsen@gmail.com>
2021-09-28 18:48:14 +0200

    moved tailwind files in

---
+++ pages/test.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default function TestPage() {
+  return (
+      <body className="bg-red-500 px-10 py-40 text-center dark:bg-red-600">
+          <h1>hej</h1>
+      </body>
+  )
+}
