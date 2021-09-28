a414afd3780622f55b3ce12b38843182f102777d
rilarchsen <rilarchsen@gmail.com>
2021-09-28 18:55:24 +0200

    initialized website with tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
 import Head from 'next/head'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Crkjdsjksdhjkeate Next App</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <Link href="test">
-            <a className="text-blue-600" href="">
-              Next.js!
-            </a>
-          </Link>
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js!
+          </a>
         </h1>
 
         <p className="mt-3 text-2xl">
